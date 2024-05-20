b0c6371eabc95982579068cf0ed57e1113869667
Jaime Mercado <3497128+s4lly@users.noreply.github.com>
2024-05-20 06:44:42 -0700

    basic dest list (#5)

---
+++ src/Route/GSuggestions.js
@@ -0,0 +1,84 @@
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import useOnclickOutside from "react-cool-onclickoutside";
+import { useContext } from "react";
+import { Context } from "../Context";
+
+const GSuggestions = () => {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      /* Define search scope here */
+    },
+    debounce: 300,
+  });
+  const { dispatch } = useContext(Context);
+
+  const ref = useOnclickOutside(() => {
+    // When user clicks outside of the component, call it to clear and reset the suggestions data
+    clearSuggestions();
+  });
+
+  const handleInput = (e) => {
+    // Update the keyword of the input element
+    setValue(e.target.value);
+  };
+
+  const handleSelect = (suggestion) => {
+    return async () => {
+      const { description, place_id } = suggestion;
+
+      // When user selects a place, we can replace the keyword without request data from API
+      // by setting the second parameter to "false"
+      setValue("", false);
+      clearSuggestions();
+
+      // Get latitude and longitude via utility functions
+      const results = await getGeocode({ address: description });
+      const { lat, lng } = getLatLng(results[0]);
+
+      console.log("📍 Coordinates: ", { lat, lng });
+
+      dispatch({
+        type: "stops:list:add",
+        payload: { name: description, gMapsData: { place_id, lat, lng } },
+      });
+    };
+  };
+
+  const renderSuggestions = () =>
+    data.map((suggestion) => {
+      const {
+        place_id,
+        structured_formatting: { main_text, secondary_text },
+      } = suggestion;
+
+      return (
+        <li key={place_id} onClick={handleSelect(suggestion)}>
+          <strong>{main_text}</strong> <small>{secondary_text}</small>
+        </li>
+      );
+    });
+
+  return (
+    <div ref={ref}>
+      <input
+        value={value}
+        onChange={handleInput}
+        disabled={!ready}
+        placeholder="Where are you going?"
+      />
+      {/* We can use the "status" to decide whether we should display the dropdown or not */}
+      {status === "OK" && <ul>{renderSuggestions()}</ul>}
+    </div>
+  );
+};
+
+export default GSuggestions;
---
+++ src/Route/index.js
@@ -0,0 +1,13 @@
+import GSuggestions from "./GSuggestions";
+
+const Search = () => {
+  return (
+    <div>
+      <h2>Search</h2>
+
+      <GSuggestions />
+    </div>
+  );
+};
+
+export default Search;
