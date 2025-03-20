5317649323b679fc4da365f1d5b7ec89a59bda4e
s4lly <3497128+s4lly@users.noreply.github.com>
2025-03-20 00:14:00 -0700

    move to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { getTrips } from "./actions";
 import { Suspense } from "react";
 import Link from "next/link";
 
+import clsx from "clsx";
+
 import classes from "@/app/page.module.css";
 import MyTrips from "@/components/my-trips/my-trips";
 
@@ -24,12 +26,36 @@ export default async function Home() {
 
   return (
     <StateProvider>
-      <div className="content">
-        <div className="app">
+      <header>
+        <Link href="/">
+          <h1 className="mb-4 text-4xl hover:underline">split trip gas</h1>
+        </Link>
+
+        <div
+          className={clsx(
+            "flex justify-between items-center",
+            !auth.user && "flex-row-reverse"
+          )}
+        >
+          {auth.user && <div>{auth.user.email}</div>}
+
+          <nav>
+            <ul className={classes.nav}>
+              {auth.user ? (
+                <Link href="/logout">logout</Link>
+              ) : (
+                <Link href="/login">Login</Link>
+              )}
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <div className="">
+        <div className="">
           {isUserLoggedIn && (
             <div>
               <div className={classes.tripsHeader}>
-                <h2>My Trips</h2>
+                <h2 className="text-2xl mb-2">My Trips</h2>
                 <Link href="/trips/new">new</Link>
               </div>
               <Suspense fallback={<div>Loading...</div>}>
