3008d815bba35cba51610e544207fa6a623ba6df
s4lly <3497128+s4lly@users.noreply.github.com>
2025-04-02 17:42:17 -0700

    add home page back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,24 @@
 import Link from "next/link";
-import { redirect } from "next/navigation";
 import { StateProvider } from "@/components/Context";
 import LayoutDrawer from "@/components/layout-drawer";
 import MyTrips from "@/components/my-trips/my-trips";
-import { createClient } from "@/utils/supabase/server";
 
 export default async function Home() {
-  const supabase = await createClient();
-
-  const { data, error } = await supabase.auth.getUser();
-  if (error || !data?.user) {
-    redirect("/login");
-  }
-
   return (
     <StateProvider>
-      <h1>split trip gas between friends</h1>
-      <p>Hello {data.user.email}</p>
+      <header>
+        <nav className="flex items-center justify-between">
+          <div>
+            <Link href="/">
+              <h1 className="text-4xl font-extrabold">split trip gas</h1>
+            </Link>
+          </div>
+          <LayoutDrawer />
+        </nav>
+      </header>
+      <section>
+        <MyTrips />
+      </section>
     </StateProvider>
   );
 }
