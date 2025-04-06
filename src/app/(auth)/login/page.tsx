a55bca60d02baf48380be74d6d4179dbe91f4c3d
s4lly <3497128+s4lly@users.noreply.github.com>
2025-04-05 21:46:15 -0700

    update login sign up page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,21 +1,61 @@
-import classes from "@/app/(auth)/login/login.module.css";
+import { Label } from "@radix-ui/react-label";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import { login, signup } from "./actions";
 
 export default function LoginPage() {
   return (
-    <form className={classes.inputContainer}>
-      <div className={classes.input}>
-        <label htmlFor="email">Email:</label>
-        <input id="email" name="email" type="email" required />
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <div className="flex flex-col gap-6">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-2xl">Login</CardTitle>
+              <CardDescription>
+                Enter your email below to login to your account
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="flex flex-col gap-6">
+                  <div className="grid gap-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="m@example.com"
+                      required
+                    />
+                  </div>
+                  <div className="grid gap-2">
+                    <div className="flex items-center">
+                      <Label htmlFor="password">Password</Label>
+                    </div>
+                    <Input id="password" type="password" required />
+                  </div>
+                  <Button formAction={login} className="w-full">
+                    Login
+                  </Button>
+                  <Button
+                    formAction={signup}
+                    variant="secondary"
+                    className="w-full"
+                  >
+                    Sign up
+                  </Button>
+                </div>
+              </form>
+            </CardContent>
+          </Card>
+        </div>
       </div>
-
-      <div className={classes.input}>
-        <label htmlFor="password">Password:</label>
-        <input id="password" name="password" type="password" required />
-      </div>
-
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    </div>
   );
 }
